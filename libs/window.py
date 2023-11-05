7a4ad56fcd3f326ce44f11d3bc359dd111404756
RepubblicaTech <elshorbagy.omar@gozzi-olivetti.org>
2023-11-05 19:07:23 +0100

    Create CTK window

---
+++ libs/window.py
@@ -0,0 +1,7 @@
+import customtkinter as CTk
+
+
+class Window:
+    def __init__(self, title="CustomTkinter"):
+        self = CTk.CTk()
+        self.title = str("Title")
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import customtkinter as CTk
+import libs.window as window
 
 CTk.set_appearance_mode("System")  # Modes: system (default), light, dark
 CTk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+mainWindow = window.Window("Apple86 Reborn")
