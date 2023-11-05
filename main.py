ab6ecc85d044e1169535a680a4e07db9985fb44e
RepubblicaTech <elshorbagy.omar@gozzi-olivetti.org>
2023-11-05 19:02:18 +0100

    Upload files to repo

---
+++ main.py
@@ -0,0 +1,4 @@
+import customtkinter as CTk
+
+CTk.set_appearance_mode("System")  # Modes: system (default), light, dark
+CTk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
