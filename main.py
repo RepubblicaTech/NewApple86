a56251a762de944587ac720f7befcd6c579011d5
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2023-11-06 20:39:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import customtkinter as CTk
 import libs.window as window
 
-
 def log(text="Hello"):
     print(text)
-
 
 CTk.set_appearance_mode("System")  # Modes: system (default), light, dark
 CTk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
@@ -16,8 +14,7 @@ mainWindow = window.Window("Apple86 Reborn",
 
 mainWindow.button = CTk.CTkButton(mainWindow,
                                   text='OMOa',
-                                  command=lambda: log('Error 0x02E1: Button clicked'))
-mainWindow.button.grid(padx=20,
-                       pady=20)
+                                  command=lambda: log('Button was clicked')).grid(padx=20,
+                                                                                  pady=20)
 
 mainWindow.mainloop()
